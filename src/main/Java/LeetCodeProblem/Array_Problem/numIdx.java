fb4fb045ad96454f3038d0cc864fcf28e9fc6c69
BangjinHu <bangjinhu.com.cn@gmail.com>
2022-10-12 23:59:46 +0800

    update some problem

---
+++ src/main/Java/LeetCodeProblem/Array_Problem/numIdx.java
@@ -0,0 +1,30 @@
+package LeetCodeProblem.Array_Problem;
+
+public class numIdx {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int numIdx(int[] nums, int target) {
+
+        // 有序数组/返回重复元素最后下标
+        int low = 0, high = nums.length - 1;
+        int idx = 0;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            idx = mid;
+            if (nums[mid] > target) {
+                high = mid;
+            } else if (nums[mid] == target && nums[idx] == nums[idx + 1]) {
+                idx += 1;
+            } else {
+                low = mid + 1;
+            }
+            if (idx != 0) {
+                return idx;
+            }
+        }
+        return low;
+    }
+}
